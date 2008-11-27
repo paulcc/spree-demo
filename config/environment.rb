a2b881547d7de5003f5f06b1a8791a4842a26849
Sean Schofield <schof@apache.org>
2008-11-26 20:36:57 -0500

    First commit.

---
+++ config/environment.rb
@@ -0,0 +1,105 @@
+# Be sure to restart your web server when you modify this file.
+
+# Uncomment below to force Rails into production mode when 
+# you don't control web/app server and can't set it the proper way
+# ENV['RAILS_ENV'] ||= 'production'
+
+# Specifies gem version of Rails to use when vendor/rails is not present
+SPREE_GEM_VERSION = '0.5.99' unless defined? SPREE_GEM_VERSION
+RAILS_GEM_VERSION = "2.1.2" unless defined? RAILS_GEM_VERSION
+          
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+
+Spree::Initializer.run do |config|
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  # See Rails::Configuration for more options.
+
+  # Skip frameworks you're not going to use. To use Rails without a database
+  # you must remove the Active Record framework.
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+
+  # Specify gems that this application depends on. 
+  # They can then be installed with "rake gems:install" on new installations.
+  # config.gem "bj"
+  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+  # config.gem "aws-s3", :lib => "aws/s3"
+
+  config.gem "has_many_polymorphs", :version => '2.12'
+  config.gem "highline", :version => '1.4.0'
+  config.gem "mini_magick", :version => '1.2.3'
+  config.gem "activemerchant", :lib => "active_merchant", :version => '1.3.2'
+  config.gem "tlsmail"
+  config.gem 'active_presenter', :version => '0.0.4'
+
+  # Only load the plugins named here, in the order given. By default, all plugins 
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  config.plugins = [ :all, :resource_controller, :extension_patches ] 
+  
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/extras )
+
+  # Force all environments to use the same logger level
+  # (by default production uses :info, the others :debug)
+  # config.log_level = :debug
+
+  # Make Time.zone default to the specified zone, and make Active Record store time values
+  # in the database in UTC, and return them converted to the specified local zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. 
+  config.time_zone = "Eastern Time (US & Canada)"
+
+  # Your secret key for verifying cookie session data integrity.
+  # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random, 
+  # no regular words or you'll be exposed to dictionary attacks.
+  config.action_controller.session = {
+    :session_key => '_spree-demo_session',
+    :secret      => '0ffffec16cfdac200f6142ea8fd13c0156b6042e4d35769a13883ab3234978245aaa77d83be8948ef53a455dcb4df3038622d85c857fe30ad778f494c3143f35' 
+  }
+
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
+  # (create the session table with "rake db:sessions:create")
+  # config.action_controller.session_store = :active_record_store
+
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper,
+  # like if you have constraints or database-specific column types
+  config.active_record.schema_format = :sql
+
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector  
+end
+
+
+# Load extension gems.
+config = Rails::Configuration.new
+Object.subclasses_of(Spree::Extension).each do |extension|
+  if extension.respond_to? :require_gems
+    extension.require_gems(config)
+  end
+end
+
+config.gems.each do |gem|
+  gem.add_load_paths
+  gem.load
+end
+
+# Add new inflection rules using the following format 
+# (all these examples are active by default):
+# Inflector.inflections do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+# Mime::Type.register "application/x-mobile", :mobile
+
+# Include your application configuration below
---
+++ config/initializers/spree.rb
@@ -0,0 +1,40 @@
+# Spree Configuration
+SESSION_KEY = '_spree_session_id'
+FLAT_SHIPPING_RATE = 10 # applies only to the flat rate shipping option
+
+#require 'spree/support/core_ext/array/conversions'
+
+# TODO - Add the lib/plugins stuff maybe?
+
+# Initialize preference system (and attribute_fu)
+#
+# We need to hand-load attribute_fu stuff here because we call
+# ActiveRecord#has_many method before the attribute_fu plugin is
+# completely loaded.
+ActiveRecord::Base.class_eval do
+  include AttributeFu::Associations
+  include Spree::Preferences
+  include Spree::Preferences::ModelHooks
+end
+
+ActionView::Helpers::FormBuilder.class_eval do
+  include AttributeFu::AssociatedFormHelper
+end
+
+
+# Initialize mail server settings
+Spree::Preferences::MailSettings.init
+
+# Add extra support goodies (similar to rails active support)
+#class Array #:nodoc:
+#  include Spree::Support::CoreExtensions::Array
+#end
+
+class String #:nodoc:
+  include Spree::Support::CoreExtensions::String
+end
+
+CalendarDateSelect.format = :american
+
+Locale.code = Spree::Config[:default_locale]
+  
