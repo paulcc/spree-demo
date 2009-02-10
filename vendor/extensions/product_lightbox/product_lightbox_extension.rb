14acb10f8ef28317083f86315ec7de502fbc63ae
Sean Schofield <schof@apache.org>
2009-02-10 12:56:48 -0500

    Added Steph's lightbox extension.

---
+++ vendor/extensions/product_lightbox/product_lightbox_extension.rb
@@ -0,0 +1,11 @@
+# Uncomment this if you reference any of your controllers in activate
+# require_dependency 'application'
+
+class ProductLightboxExtension < Spree::Extension
+  version "1.0"
+  description "Product Lightbox Extension"
+  url "http://www.endpoint.com"
+
+  def activate
+  end
+end
