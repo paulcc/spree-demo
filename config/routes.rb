8317cbc8c806e8aa2f996147f70aaf5533ecc04c
Sean Schofield <schof@apache.org>
2009-03-23 13:56:08 -0400

    Added route for reports.

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,8 @@ ActionController::Routing::Routes.draw do |map|
     admin.resource :general_settings
     admin.resources :taxonomies do |taxonomy|
       taxonomy.resources :taxons
-    end
+    end 
+    admin.resources :reports, :only => [:index, :show], :collection => {:sales_total => :get}
   end
   
   # Install the default route as the lowest priority.
