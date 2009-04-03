cf6d8c4680a9edaee8922782147392b0d5b674f6
Sean Schofield <schof@apache.org>
2009-04-03 16:32:22 -0400

    More missing edge files.

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,24 +1,13 @@
 var regions = new Array('billing', 'shipping', 'shipping_method', 'creditcard', 'confirm_order');
 
+/*
 $(document).ajaxSend(function(event, request, settings) {
   if (typeof(AUTH_TOKEN) == "undefined") return;
   // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
   settings.data = settings.data || "";
   settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
 });
-
-// public/javascripts/application.js
-jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
-jQuery.fn.submitWithAjax = function() {
-  this.change(function() {
-    $.post('select_country', $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
+*/
 
 jQuery.fn.sameAddress = function() {
   this.click(function() {
@@ -26,70 +15,97 @@ jQuery.fn.sameAddress = function() {
       //Clear ship values?
       return;
     }
-    $('span#scountry select').val($('span#bcountry select').val());
-    update_state('s');
+    $('input#hidden_sstate').val($('input#hidden_bstate').val());
     $("#billing input, #billing select").each(function() {
       $("#shipping #"+ $(this).attr('id').replace('bill', 'ship')).val($(this).val());
     })
-    //For some reason this isn't getting picked up from above.. Debug later
-    $('#sstate :child').val($('#bstate :child').val());
+    update_state('s');
   })
 }
 
 //On page load
 $(function() {  
-  //$("#checkout_presenter_bill_address_country_id").submitWithAjax();  
   $('#checkout_presenter_same_address').sameAddress();
-  $('span#bcountry select').change(function() { $('input#hidden_bstate').val(''); update_state('b'); });
-  $('span#scountry select').change(function() { $('input#hidden_sstate').val(''); update_state('s'); });
+  $('span#bcountry select').change(function() { update_state('b'); });
+  $('span#scountry select').change(function() { update_state('s'); });
   get_states();
-
   $('#validate_billing').click(function() { if(validate_section('billing')) { submit_billing(); }});
   $('#validate_shipping').click(function() { if(validate_section('shipping')) { submit_shipping(); }});
   $('#select_shipping_method').click(function() { submit_shipping_method(); });  
   $('#confirm_payment').click(function() { if(validate_section('creditcard')) { confirm_payment(); }});
   $('form#checkout_form').submit(function() { return !($('div#confirm_order').hasClass('checkout_disabled')); }); 
-  // indicates order is ready to be processed (as opposed to simply updated)
-  $("div#creditcard div.inner").hide(function() { $("input#final_answer").attr("value", ""); });
-  $("div#creditcard div.inner").show(function() { $("input#final_answer").attr("value", "yes"); });  
 })
 
 //Initial state mapper on page load
 var state_mapper;
 var get_states = function() {
-  $.getJSON('/javascripts/states.js', function(json) {
+  $.getJSON('/states.js', function(json) {
     state_mapper = json;
-    $('span#bcountry select').val($('[name=submit_bcountry]').val());
+    $('span#bcountry select').val($('input#hidden_bcountry').val());
     update_state('b');
-    $('span#bstate :child').val($('[name=submit_bstate]').val());
-    $('span#scountry select').val($('[name=submit_scountry]').val());
+    $('span#bstate :only-child').val($('input#hidden_bstate').val());
+    $('span#scountry select').val($('input#hidden_scountry').val());
     update_state('s');
-    $('span#sstate :child').val($('[name=submit_sstate]').val());
+    $('span#sstate :only-child').val($('input#hidden_sstate').val());
   });
 };
 
-//Update state input / select
+// replace the :only child of the parent with the given html, and transfer
+//   {name,id} attributes over, returning the new child
+var chg_state_input_element = function (parent, html) {
+  var child = parent.find(':only-child');
+  var name = child.attr('name');
+  var id = child.attr('id');
+  //Toggle back and forth between id and name
+  if(html.attr('type') == 'text' && child.attr('type') != 'text') {
+    name = name.replace('_id', '_name');
+    id = id.replace('_id', '_name');
+  } else if(html.attr('type') != 'text' && child.attr('type') == 'text') {
+    name = name.replace('_name', '_id');
+    id = id.replace('_name', '_id');
+  }
+  html.addClass('required')
+      .attr('name', name)
+      .attr('id',   id);
+  child.remove();		// better as parent-relative?
+  parent.append(html);
+  return html;
+};
+
+
+// TODO: better as sibling dummy state ?
+// Update the input method for address.state 
 var update_state = function(region) {
-  var name = $('span#' + region + 'state :child').attr('name');
-  var id = $('span#' + region + 'state :child').attr('id');
-  $('span#' + region + 'state :child').remove();
-  var match;
-  var selected = $('span#' + region + 'country :child :selected').html();
-  $.each(state_mapper.maps, function(i, item) {
-    if(selected == item.country) {
-      match = item.states;
-    }
-  });
-  if(match) {
-    $('span#' + region + 'state').append($(document.createElement('select')));
-    $.each(match, function(i, item) {
-      $('span#' + region + 'state select').append($(document.createElement('option')).attr('value', item.value).html(item.text));
+  var country        = $('span#' + region + 'country :only-child').val();
+  var states         = state_mapper[country];
+  var hidden_element = $('input#hidden_' + region + 'state');
+
+  var replacement;
+  if(states) {
+    // recreate state selection list
+    replacement = $(document.createElement('select'));
+    var states_with_blank = [["",""]].concat(states);
+    $.each(states_with_blank, function(pos,id_nm) {
+      var opt = $(document.createElement('option'))
+                .attr('value', id_nm[0])
+                .html(id_nm[1]);
+      replacement.append(opt);
+      if (id_nm[0] == hidden_element.val()) { opt.attr('selected', 'true') }
+        // set this directly IFF the old value is still valid
     });
   } else {
-    $('span#' + region + 'state').append($(document.createElement('input')));
+    // recreate an input box
+    replacement = $(document.createElement('input'));
+    if (! hidden_element.val().match(/^\d+$/)) { replacement.val(hidden_element.val()) }
   }
-  $('span#' + region + 'state select, span#' + region + 'state input').addClass('required').attr('name', name).attr('id', id).val($('input#hidden_' + region + 'state').val()); 
-  $('span#' + region + 'state select, span#' + region + 'state input').change(function() {
+
+  chg_state_input_element($('span#' + region + 'state'), replacement);
+  hidden_element.val(replacement.val());
+
+  // callback to update val when form object is changed
+  // This is only needed if we want to preserve state when someone refreshes the checkout page
+  // Or... if someone changes between countries with no given states
+  replacement.change(function() {
     $('input#hidden_' + region + 'state').val($(this).val());
   });
 };
@@ -106,6 +122,7 @@ var validate_section = function(region) {
 };
 
 var shift_to_region = function(active) {
+  $('div#flash-errors').remove();  
   var found = 0;
   for(var i=0; i<regions.length; i++) {
     if(!found) {
@@ -124,6 +141,12 @@ var shift_to_region = function(active) {
       $('div#' + regions[i] + ' div.inner').hide('fast');
       $('div#' + regions[i]).addClass('checkout_disabled');
     }
+  }                                                                         
+  if (active == 'confirm_order') {
+    $("input#final_answer").attr("value", "yes");    
+  } else {
+    // indicates order is ready to be processed (as opposed to simply updated)
+    $("input#final_answer").attr("value", "");    
   }
   return;
 };
@@ -136,7 +159,7 @@ var submit_billing = function() {
 
 var build_address = function(title, region) {
   var address = '<h3>' + title + '</h3>';
-  address += $('p#' + region + 'fname input').val() + ' ' + $('p#blname input').val() + '<br />';
+  address += $('p#' + region + 'fname input').val() + ' ' + $('p#' + region + 'lname input').val() + '<br />';
   address += $('p#' + region + 'address input').val() + '<br />';
   if($('p#' + region + 'address2').val() != '') {
     address += $('p#' + region + 'address2').val() + '<br />';
@@ -165,7 +188,7 @@ var submit_shipping = function() {
       xhr.setRequestHeader('Accept-Encoding', 'identity');
     },      
     dataType: "json",
-    data: "{form: '" + $('#checkout_form').serialize() + "'}",
+    data: $('#checkout_form').serialize(),
     success: function(json) {  
       update_shipping_methods(json.available_methods); 
     },
@@ -196,7 +219,7 @@ var submit_shipping_method = function() {
         xhr.setRequestHeader('Accept-Encoding', 'identity');
       },      
       dataType: "json",
-      data: "{form: '" + $('#checkout_form').serialize() + "'}",
+      data: $('#checkout_form').serialize(),
       success: function(json) {  
         update_confirmation(json.order); 
       },
@@ -218,18 +241,21 @@ var update_shipping_methods = function(methods) {
     $('div$methods img#shipping_loader').remove();
     var p = document.createElement('p');
     var s = this.name + ' ' + this.rate;
-    $(p).append($(document.createElement('input'))
-                .attr('id', s)
+    var i = $(document.createElement('input'))
+                .attr('id', this.id)
                 .attr('type', 'radio')
                 .attr('name', 'method_id')
-                .attr(1 == $(methods).length ? 'checked' : 'notchecked', 'foo')
                 .val(this.id)
-                );
-    $(p).append($(document.createElement('label'))
+                .click(function() { $('div#methods input').attr('checked', ''); $(this).attr('checked', 'checked'); });
+    if($(methods).length == 1) {
+      i.attr('checked', 'checked');
+    }
+    var l = $(document.createElement('label'))
                 .attr('for', s)
                 .html(s)
-                .css('top', '-1px'));
-    $('div#methods').append(p);
+                .css('top', '-1px')
+                .css('width', '300px');
+    $('div#methods').append($(p).append(i).append(l));
   });
   $('div#methods input:first').attr('validate', 'required:true');
   return;
